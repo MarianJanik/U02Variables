dfaeefd75fb98dead48ea1cb9aa942e79eee22a1
Marian Janik <marian.janik@seznam.cz>
2021-08-02 22:35:02 +0200

    copy all file

---
+++ src/com/engeto/examples/Main.java
@@ -0,0 +1,19 @@
+package com.engeto.examples;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        String name; //1
+        String surname; //1
+        int numberOfCustomers; //2
+        String city; //3
+        int consuptionPer100; //4
+        int lenghtUTP; //5
+        LocalDate dateOfBirdth; //6
+        String SPZ; //7
+        String IPaddress; //8
+    }
+}
